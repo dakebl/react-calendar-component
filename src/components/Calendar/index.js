b16311c1fc9f3e2bc79b1393a3fe7617542687a2
Dale Blackburn <dale@dakebl.co.uk>
2017-10-03 23:17:43 +0100

    installed Moment.js and used it to give an initial state to the Calendar component

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/en-gb';
 
 import CalendarNav from './CalendarNav';
 import CalendarGrid from './CalendarGrid';
 
 class Calendar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    let today = moment();
+
+    this.state = {
+      today: today
+    };
+
+  }
   render() {
     return (
       <div className="Calendar">
