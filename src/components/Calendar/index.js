ee54d425e9b885e3ec4a7f61c4f30c14eb5a8b39
Dale Blackburn <dale@dakebl.co.uk>
2017-10-03 23:57:29 +0100

    updated intiial state to include the current selected month view and the dates for that view. Created a generateDates function to calculate the correct dates for the view and called it from componentWillMount

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -9,13 +9,42 @@ class Calendar extends React.Component {
   constructor(props) {
     super(props)
 
-    let today = moment();
-
     this.state = {
-      today: today
+      today: moment(),
+      current: moment(),
+      dates: []
     };
 
   }
+
+  componentWillMount() {  
+    this.generateDates();
+  }
+
+  generateDates() {
+    let dates = [],
+        weekNumber = 0,
+        firstDayOfTheMonth = this.state.current.clone().startOf(`month`).startOf(`week`);
+
+    while(dates.length < 42) { 
+
+      if ( firstDayOfTheMonth.format('ddd') === 'Mon' ) { weekNumber++; }
+      
+      dates.push({ 
+        date: firstDayOfTheMonth.date(),
+        week: weekNumber,
+        day: firstDayOfTheMonth.day(),
+        inMonth: (firstDayOfTheMonth.format('M') === this.state.current.format('M')) ? 1 : 0,
+        isToday: (firstDayOfTheMonth.clone().startOf('date').diff(moment().startOf('date')) === 0) ? 1 : 0
+      });
+      
+      firstDayOfTheMonth.add(1, 'days');
+    }
+    
+    this.setState({ dates: dates });
+  }
+
+
   render() {
     return (
       <div className="Calendar">
